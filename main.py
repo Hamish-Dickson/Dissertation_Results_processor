ee34f351063a5825b6ade0bd572a863f966df494
Hamish <Hamishdickson98@gmail.com>
2020-03-12 18:12:01 +0000

    added weighted plot method for comparing fitness methods

--- main.py
+++ main.py
@@ -28,17 +28,26 @@ def getVals():
     aggressiveMutate_improve = read('C:\\Users\\bktzg\\aggressive improves.csv')
     aggressiveMutate_average = read('C:\\Users\\bktzg\\aggressive averages.csv')
 
+    w8020_improve = read('C:\\Users\\bktzg\\8020 improves.csv')
+
+    w9010_improve = read('C:\\Users\\bktzg\\9010 improves.csv')
+
+    w7030_improve = read('C:\\Users\\bktzg\\7030 improves.csv')
+
+    w6040_improve = read('C:\\Users\\bktzg\\6040 improves.csv')
+
     return baseline_improve, twoPoint_improve, multiPoint_improve, aggressiveMutate_improve, baseline_average, \
-           twoPoint_average, multiPoint_average, aggressiveMutate_average
+           twoPoint_average, multiPoint_average, aggressiveMutate_average, w8020_improve, w9010_improve, \
+           w7030_improve, w6040_improve
 
 
 def plot(baseline, twopoint, multipoint, aggressive):
-    plt.plot(baseline, label='baseline')
-    plt.plot(twopoint, label='two-point crossover')
-    plt.plot(multipoint, label='multi-point mutator')
-    plt.plot(aggressive, label='aggressive mutator')
+    plt.plot(baseline, label='Baseline')
+    plt.plot(twopoint, label='Two Point Crossover')
+    plt.plot(multipoint, label='Multi Point Mutation')
+    plt.plot(aggressive, label='Aggressive Mutation')
     plt.xlabel('Generation')
-    plt.ylabel('Maximum Score')
+    plt.ylabel('Fitness score')
     plt.title('Comparison of maximum score through iterations between\n various GA operators')
     plt.legend()
     plt.autoscale()
@@ -52,23 +61,49 @@ def boxplot(baseline_average_results, twoPoint_average_results, multiPoint_avera
     axs[0, 0].boxplot(baseline_average_results)
     axs[0, 0].set_title('Baseline \nStandard Deviation:\n' + str(np.std(baseline_average_results, dtype=np.float64)))
     axs[0, 0].set_ylabel('Fitness score')
+    axs[0, 0].set(ylim=(35, 50))
 
     axs[0, 1].boxplot(twoPoint_average_results)
     axs[0, 1].set_title(
-        'Two point crossover\nStandard Deviation:\n' + str(np.std(twoPoint_average_results, dtype=np.float64)))
+        'Two Point Crossover\nStandard Deviation:\n' + str(np.std(twoPoint_average_results, dtype=np.float64)))
     axs[0, 1].set_ylabel('Fitness score')
+    axs[0, 1].set(ylim=(35, 50))
 
     axs[1, 0].boxplot(multiPoint_average_results)
     axs[1, 0].set_title(
-        'Multi point mutation\nStandard Deviation:\n' + str(np.std(multiPoint_average_results, dtype=np.float64)))
+        'Multi Point Mutation\nStandard Deviation:\n' + str(np.std(multiPoint_average_results, dtype=np.float64)))
     axs[1, 0].set_ylabel('Fitness score')
+    axs[1, 0].set(ylim=(35, 50))
 
     axs[1, 1].boxplot(aggressiveMutate_average_results)
     axs[1, 1].set_title(
-        'Aggressive mutation\nStandard Deviation:\n' + str(np.std(aggressiveMutate_average_results, dtype=np.float64)))
+        'Aggressive Mutation\nStandard Deviation:\n' + str(np.std(aggressiveMutate_average_results, dtype=np.float64)))
     axs[1, 1].set_ylabel('Fitness score')
+    axs[1, 1].set(ylim=(35, 50))
 
-    plt.autoscale()
+    # plt.autoscale()
+
+    plt.show()
+
+
+def weighted_plot(w8020_results, w9010_results, w7030_results, w6040_results):
+    for i in range(0, len(w8020_results)):
+        w8020_results[i] = w8020_results[i] / 104.2 * 100
+        w9010_results[i] = w9010_results[i] / 90.1 * 100
+        w7030_results[i] = w7030_results[i] / 118.3 * 100
+        w6040_results[i] = w6040_results[i] / 132.4 * 100
+
+    plt.plot(w9010_results, label='90% evaluation, 10% distance')
+    plt.plot(w8020_results, label='80% evaluation, 20% distance')
+    plt.plot(w7030_results, label='70% evaluation, 30% distance')
+    plt.plot(w6040_results, label='60% evaluation, 40% distance')
+    plt.xlabel('Generation')
+    plt.ylabel('% of Global maximum fitness achieved')
+    plt.title('Comparison of % of Global Maximum achievable fitness\n achieved through iterations\n'
+              'between various GA operators')
+
+    plt.ylim(25,75)
+    plt.legend()
 
     plt.show()
 
@@ -83,7 +118,14 @@ twoPoint_average_results = getVals()[5]
 multiPoint_average_results = getVals()[6]
 aggressiveMutate_average_results = getVals()[7]
 
+w8020_results = getVals()[8]
+w9010_results = getVals()[9]
+w7030_results = getVals()[10]
+w6040_results = getVals()[11]
+
 plot(baseline_improve_results, twoPoint_improve_results, multiPoint_improve_results, aggressiveMutate_improve_results)
 
 boxplot(baseline_average_results, twoPoint_average_results, multiPoint_average_results,
         aggressiveMutate_average_results)
+
+weighted_plot(w8020_results, w9010_results, w7030_results, w6040_results)
