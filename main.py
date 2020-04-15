3014efc67be2dbf41818fd5218fc92189681b83c
Hamish <Hamishdickson98@gmail.com>
2020-04-15 07:17:56 +0100

    Final commit, submitted

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ def getVals():
     aggressiveMutate_average = read('C:\\Users\\bktzg\\aggressive averages.csv')
 
     w1000_improve = read('C:\\Users\\bktzg\\1000 improves.csv')
+    w9901_improve = read('C:\\Users\\bktzg\\9901 improves.csv')
     w9010_improve = read('C:\\Users\\bktzg\\9010 improves.csv')
     w8020_improve = read('C:\\Users\\bktzg\\8020 improves.csv')
     w7030_improve = read('C:\\Users\\bktzg\\7030 improves.csv')
@@ -41,6 +42,7 @@ def getVals():
     w0100_improve = read('C:\\Users\\bktzg\\0100 improves.csv')
 
     w1000_average = read('C:\\Users\\bktzg\\1000 averages.csv')
+    w9901_average = read('C:\\Users\\bktzg\\9901 averages.csv')
     w9010_average = read('C:\\Users\\bktzg\\9010 averages.csv')
     w8020_average = read('C:\\Users\\bktzg\\8020 averages.csv')
     w7030_average = read('C:\\Users\\bktzg\\7030 averages.csv')
@@ -52,11 +54,19 @@ def getVals():
     w1090_average = read('C:\\Users\\bktzg\\1090 averages.csv')
     w0100_average = read('C:\\Users\\bktzg\\0100 averages.csv')
 
+    surrogateBaseline_improves = read('C:\\Users\\bktzg\\baselineSurrogate improves.csv')
+    surrogateBaseline_averages = read('C:\\Users\\bktzg\\baselineSurrogate averages.csv')
+    surrogate_improves = read('C:\\Users\\bktzg\\surrogate improves.csv')
+    surrogate_averages = read('C:\\Users\\bktzg\\surrogate averages.csv')
+
     return baseline_improve, twoPoint_improve, multiPoint_improve, aggressiveMutate_improve, baseline_average, \
-           twoPoint_average, multiPoint_average, aggressiveMutate_average, w1000_improve, w9010_improve, w8020_improve, \
+           twoPoint_average, multiPoint_average, aggressiveMutate_average, w1000_improve, w9901_improve, w9010_improve, \
+           w8020_improve, \
            w7030_improve, w6040_improve, w5050_improve, w4060_improve, w3070_improve, w2080_improve, w1090_improve, \
-           w0100_improve, w1000_average, w9010_average, w8020_average, w7030_average, w6040_average, w5050_average, \
-           w4060_average, w3070_average, w2080_average, w1090_average, w0100_average
+           w0100_improve, w1000_average, w9901_average, w9010_average, w8020_average, w7030_average, w6040_average, \
+           w5050_average, \
+           w4060_average, w3070_average, w2080_average, w1090_average, w0100_average, surrogateBaseline_improves, \
+           surrogate_improves, surrogateBaseline_averages, surrogate_averages
 
 
 def plot(baseline, twopoint, multipoint, aggressive):
@@ -89,31 +99,29 @@ def plot_averages(baseline, twopoint, multipoint, aggressive):
 
 def boxplot(baseline_average_results, twoPoint_average_results, multiPoint_average_results,
             aggressiveMutate_average_results):
-    fig, axs = plt.subplots(2, 2)
-    axs[0, 0].boxplot(baseline_average_results)
-    axs[0, 0].set_title('Baseline \nStandard Deviation:\n' + str(np.std(baseline_average_results, dtype=np.float64)))
-    axs[0, 0].set_ylabel('Fitness score')
-    axs[0, 0].set(ylim=(40, 50))
+    data = [baseline_average_results, twoPoint_average_results,
+            multiPoint_average_results, aggressiveMutate_average_results]
 
-    axs[0, 1].boxplot(twoPoint_average_results)
-    axs[0, 1].set_title(
-        'Two Point Crossover\nStandard Deviation:\n' + str(np.std(twoPoint_average_results, dtype=np.float64)))
-    axs[0, 1].set_ylabel('Fitness score')
-    axs[0, 1].set(ylim=(40, 50))
+    labels = ['Baseline \nStandard Deviation:\n' + str(np.std(baseline_average_results, dtype=np.float64)),
+              'Two Point Crossover\nStandard Deviation:\n' + str(np.std(twoPoint_average_results, dtype=np.float64)),
+              'Multi Point Mutation\nStandard Deviation:\n' + str(np.std(multiPoint_average_results, dtype=np.float64)),
+              'Aggressive Mutation\nStandard Deviation:\n' + str(
+                  np.std(aggressiveMutate_average_results, dtype=np.float64))]
+    plt.rcParams["font.size"] = "8"
+    fig, axs = plt.subplots()
 
-    axs[1, 0].boxplot(multiPoint_average_results)
-    axs[1, 0].set_title(
-        'Multi Point Mutation\nStandard Deviation:\n' + str(np.std(multiPoint_average_results, dtype=np.float64)))
-    axs[1, 0].set_ylabel('Fitness score')
-    axs[1, 0].set(ylim=(40, 50))
+    axs.set_xticklabels(labels)
+    plt.rcParams["font.size"] = "6"
+    box_plot = axs.boxplot(data, showmeans=True)
 
-    axs[1, 1].boxplot(aggressiveMutate_average_results)
-    axs[1, 1].set_title(
-        'Aggressive Mutation\nStandard Deviation:\n' + str(np.std(aggressiveMutate_average_results, dtype=np.float64)))
-    axs[1, 1].set_ylabel('Fitness score')
-    axs[1, 1].set(ylim=(40, 50))
+    h, l = axs.get_legend_handles_labels()
+    h.append(box_plot.get('means')[0])
+    h.append(box_plot.get('medians')[0])
+    h.append(box_plot.get('fliers')[0])
 
-    # plt.autoscale()
+    l = ['Mean', 'Median', 'Outliers']
+    plt.rcParams["legend.loc"] = 'lower right'
+    plt.legend(h, l)
 
     plt.show()
 
@@ -201,13 +209,15 @@ def barchart_percentages(w1000_results, w9010_results, w8020_results, w7030_resu
     plt.show()
 
 
-def barchart_differences(w1000_average_results, w9010_average_results, w8020_average_results, w7030_average_results,
+def barchart_differences(w1000_average_results, w9901_average_results, w9010_average_results, w8020_average_results,
+                         w7030_average_results,
                          w6040_average_results, w5050_average_results, w4060_average_results, w3070_average_results,
                          w2080_average_results, w1090_average_results, w0100_average_results):
-    labels = ['100%/0%', '90%/10%', '80%/20% ', '70%/30%', '60%/40%', '50%/50%', '40%/60%', '30%/70%',
+    labels = ['100%/0%', '99%/1%', '90%/10%', '80%/20% ', '70%/30%', '60%/40%', '50%/50%', '40%/60%', '30%/70%',
               '20%/80%', '10%/90%', '0%/100%']
 
-    vals = [w1000_average_results[-1] - w1000_average_results[0], w9010_average_results[-1] - w9010_average_results[0],
+    vals = [w1000_average_results[-1] - w1000_average_results[0], w9901_average_results[-1] - w9901_average_results[0],
+            w9010_average_results[-1] - w9010_average_results[0],
             w8020_average_results[-1] - w8020_average_results[0], w7030_average_results[-1] - w7030_average_results[0],
             w6040_average_results[-1] - w6040_average_results[0], w5050_average_results[-1] - w5050_average_results[0],
             w4060_average_results[-1] - w4060_average_results[0], w3070_average_results[-1] - w3070_average_results[0],
@@ -229,6 +239,30 @@ def barchart_differences(w1000_average_results, w9010_average_results, w8020_ave
     plt.show()
 
 
+def plot_surrogate(surrogateBaseline_improves, surrogate_improves):
+    plt.plot(surrogateBaseline_improves, label='Baseline')
+    plt.plot(surrogate_improves, label='Surrogate')
+    plt.xlabel('Generation')
+    plt.ylabel('Fitness score')
+    plt.title('Comparison of maximum score through iterations between\nsurrogate and baseline evaluation')
+    plt.legend()
+    plt.autoscale()
+
+    plt.show()
+
+
+def plot_surrogate_average(surrogateBaseline_averages, surrogate_averages):
+    plt.plot(surrogateBaseline_averages, label='Baseline')
+    plt.plot(surrogate_averages, label='Surrogate')
+    plt.xlabel('Generation')
+    plt.ylabel('Fitness score')
+    plt.title('Comparison of average score through iterations between\nsurrogate and baseline evaluation')
+    plt.legend()
+    plt.autoscale()
+
+    plt.show()
+
+
 baseline_improve_results = getVals()[0]
 twoPoint_improve_results = getVals()[1]
 multiPoint_improve_results = getVals()[2]
@@ -240,28 +274,36 @@ multiPoint_average_results = getVals()[6]
 aggressiveMutate_average_results = getVals()[7]
 
 w1000_improve_results = getVals()[8]
-w9010_improve_results = getVals()[9]
-w8020_improve_results = getVals()[10]
-w7030_improve_results = getVals()[11]
-w6040_improve_results = getVals()[12]
-w5050_improve_results = getVals()[13]
-w4060_improve_results = getVals()[14]
-w3070_improve_results = getVals()[15]
-w2080_improve_results = getVals()[16]
-w1090_improve_results = getVals()[17]
-w0100_improve_results = getVals()[18]
+w9901_improve_results = getVals()[9]
+w9010_improve_results = getVals()[10]
+w8020_improve_results = getVals()[11]
+w7030_improve_results = getVals()[12]
+w6040_improve_results = getVals()[13]
+w5050_improve_results = getVals()[14]
+w4060_improve_results = getVals()[15]
+w3070_improve_results = getVals()[16]
+w2080_improve_results = getVals()[17]
+w1090_improve_results = getVals()[18]
+w0100_improve_results = getVals()[19]
 
-w1000_average_results = getVals()[19]
-w9010_average_results = getVals()[20]
-w8020_average_results = getVals()[21]
-w7030_average_results = getVals()[22]
-w6040_average_results = getVals()[23]
-w5050_average_results = getVals()[24]
-w4060_average_results = getVals()[25]
-w3070_average_results = getVals()[26]
-w2080_average_results = getVals()[27]
-w1090_average_results = getVals()[28]
-w0100_average_results = getVals()[29]
+w1000_average_results = getVals()[20]
+w9901_average_results = getVals()[21]
+w9010_average_results = getVals()[22]
+w8020_average_results = getVals()[23]
+w7030_average_results = getVals()[24]
+w6040_average_results = getVals()[25]
+w5050_average_results = getVals()[26]
+w4060_average_results = getVals()[27]
+w3070_average_results = getVals()[28]
+w2080_average_results = getVals()[29]
+w1090_average_results = getVals()[30]
+w0100_average_results = getVals()[31]
+
+surrogateBaseline_improves = getVals()[32]
+surrogate_improves = getVals()[33]
+
+surrogateBaseline_averages = getVals()[34]
+surrogate_averages = getVals()[35]
 
 plot(baseline_improve_results, twoPoint_improve_results, multiPoint_improve_results, aggressiveMutate_improve_results)
 
@@ -283,6 +325,11 @@ barchart_percentages(w1000_improve_results, w9010_improve_results, w8020_improve
                      w4060_improve_results,
                      w3070_improve_results, w2080_improve_results, w1090_improve_results, w0100_improve_results)
 
-barchart_differences(w1000_average_results, w9010_average_results, w8020_average_results, w7030_average_results,
+barchart_differences(w1000_average_results, w9901_average_results, w9010_average_results, w8020_average_results,
+                     w7030_average_results,
                      w6040_average_results, w5050_average_results, w4060_average_results, w3070_average_results,
                      w2080_average_results, w1090_average_results, w0100_average_results)
+
+plot_surrogate(surrogateBaseline_improves, surrogate_improves)
+
+plot_surrogate_average(surrogateBaseline_averages, surrogate_averages)
